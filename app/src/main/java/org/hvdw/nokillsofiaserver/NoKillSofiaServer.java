b796d33696d433b3253953e08ec10bddc0fe7c1c
hvdwolf <hvdwolf@gmail.com>
2018-01-01 15:46:15 +0100

    first commit

---
+++ app/src/main/java/org/hvdw/nokillsofiaserver/NoKillSofiaServer.java
@@ -0,0 +1,25 @@
+package org.hvdw.nokillsofiaserver;
+
+import android.util.Log;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class NoKillSofiaServer implements IXposedHookLoadPackage {
+	public static final String TAG = "NoKillSofiaServer";
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+		if (!lpparam.packageName.equals("com.syu.ms")) return;
+
+		findAndHookMethod("com.syu.ms.app.ToolkitApp", lpparam.classLoader, "killAllAppButSome", new XC_MethodHook() {
+			//@Override
+			protected void replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+				Log.d(TAG, "skipping method killAllAppButSome");
+				param.setResult(null);
+			}
+		});
+	}
+}
