81169349c645f5ec11968b172e19ba62244207e1
hvdwolf <hvdwolf@gmail.com>
2018-01-10 17:27:05 +0100

    only use internal classpatch , notcomplete classpath

--- app/src/main/java/org/hvdw/sofiaservernokill/SofiaServerNoKill.java
+++ app/src/main/java/org/hvdw/sofiaservernokill/SofiaServerNoKill.java
@@ -4,23 +4,34 @@ import android.util.Log;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class SofiaServerNoKill implements IXposedHookLoadPackage {
 	public static final String TAG = "SofiaServerNoKill";
-	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+		XposedBridge.log("Loaded app: " + lpparam.packageName);
+		if (!lpparam.packageName.equals("com.syu.ms"))
+			return;
 
-		if (!lpparam.packageName.equals("com.syu.ms")) return;
-
-		findAndHookMethod("com.syu.ms.app.ToolkitApp", lpparam.classLoader, "killAllAppButSome", new XC_MethodHook() {
+		findAndHookMethod("app.ToolkitApp", lpparam.classLoader, "killAppWhenSleep", new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-				Log.d(TAG, "skipping method killAllAppButSome");
+				XposedBridge.log("Loaded app: " + lpparam.packageName + "skipping method killAppWhenSleep");
+				Log.d(TAG, "skipping method killAppWhenSleep");
 				param.setResult(null);
-				//return;
 			}
 		});
+
+/*		findAndHookMethod("app.ToolkitApp", lpparam.classLoader, "killAllAppButSome", new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+				XposedBridge.log("Loaded app: " + lpparam.packageName + "skipping method killAllAppButSome");
+				Log.d(TAG, "skipping method killAllAppButSome");
+				param.setResult(null);
+			}
+		});
+*/
 	}
 }
